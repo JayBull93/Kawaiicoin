cb0b8e492ee8090fc9d447e294064e2d97445a44
JayBull93 <jaybull93@gmail.com>
2017-06-12 09:42:49 -0400

    Add files via upload

---
+++ wallet/rpcwallet.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2016 The Kawaiicoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef Kawaiicoin_WALLET_RPCWALLET_H
+#define Kawaiicoin_WALLET_RPCWALLET_H
+
+class CRPCTable;
+
+void RegisterWalletRPCCommands(CRPCTable &t);
+
+#endif //Kawaiicoin_WALLET_RPCWALLET_H
